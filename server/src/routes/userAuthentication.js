6e91450b448cdfc8aa3b97c5b76b4215dae663c5
Shivani <shivanikumari9979@gmail.com>
2026-06-28 17:39:07 +0530

    Authentication completed

--- server/src/routes/userAuthentication.js
+++ server/src/routes/userAuthentication.js
@@ -21,7 +21,8 @@ authRouter.get("/check", userMiddleware,(req,res)=>{
     const reply = {
         firstName:req.result.firstName,
         emailId:req.result.emailId,
-        _id: req.result._id
+        _id: req.result._id,
+        role: req.result.role,
     }
     return res.status(200).json({
         user:reply,
