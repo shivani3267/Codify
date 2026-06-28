466a2ea051948635911b4fdd97b561170bfa9e5a
Shivani <shivanikumari9979@gmail.com>
2026-06-28 17:10:13 +0530

    User model updated

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -36,9 +36,21 @@ const userSchema = new Schema({
         default:'user'
     },
     problemSolved:{
-        type:[String]
+        type:[{
+            type:Schema.Types.ObjectId,
+            ref:'problem'
+        }],
+        unique:true,
     }
+
 },{timestamps:true})
+
+//post command will execute at end when findByIdAndDelete(attached to findOneAndDelete) to delete info from submissioin as well
+userSchema.post("findOneAndDelete", async function (userInfo) {
+    if(userInfo){
+        await mongoose.model('submission').deleteMany({userId:userInfo._id})
+    }
+})
 
 const User = mongoose.model("User",userSchema)
 
