813e8c9e9e9a56edef9d3f64c37917cba4d3d976
Shivani <shivanikumari9979@gmail.com>
2026-06-29 11:16:48 +0530

    routes added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import {Provider} from 'react-redux'
-import {store} from "../src/store/store.js"
+import {store} from "./store/store.js"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
