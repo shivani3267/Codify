51a009baba7c0a30910c84191ae3b033713edc38
Shivani <shivanikumari9979@gmail.com>
2026-06-30 16:28:58 +0530

    cors setup

--- server/src/controllers/userAuthenticate.js
+++ server/src/controllers/userAuthenticate.js
@@ -33,7 +33,13 @@ export const register = async(req,res) => {
         }
     
        const token = getToken({_id:user._id, role:user.role,  emailId});
-       res.cookie('token',token, {maxAge:60*60*1000});
+    //    res.cookie('token',token, {maxAge:60*60*1000});
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge:60*60*1000
+});
     //    console.log(token)
        res.status(201).json({
             user:reply,
@@ -69,7 +75,13 @@ export const login = async (req,res) => {
         }
 
        const token = getToken({_id:user._id, role:user.role, emailId});
-       res.cookie('token',token, {maxAge:60*60*1000});
+    //    res.cookie('token',token, {maxAge:60*60*1000});
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge:60*60*1000
+});
     //    console.log(token)
        res.status(201).json({
             user:reply,
